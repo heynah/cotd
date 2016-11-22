be3baa5168e47aab36bc7e6aaf96b66acfac3601
heynah <hannanicol@gmail.com>
2016-11-22 11:38:59 -0500

    Routing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import  { render } from 'react-dom';
-import './css/style.css';
+import{ BrowserRouter, Match, Miss } from 'react-router';
 
-import StorePicker from './components/StorePicker';
+import './css/style.css';
 import App from './components/App';
 
-render(<App/>, document.querySelector('#main'));
+import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match pattern="/store/:storeId" component={App} /> 
+        <Miss component={NotFound} />       
+      </div>        
+    </BrowserRouter>      
+  )
+}
+
+
+render(<Root/>, document.querySelector('#main'));
