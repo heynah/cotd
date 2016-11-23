dde463f9f655353b6f251deb177fed63ec97935c
heynah <hannanicol@gmail.com>
2016-11-22 19:26:55 -0500

    add STATE to app (fishes) & create and pass a method (via PROPS) to the Comps

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,37 @@ import Inventory from './Inventory';
 //import  { render } from 'react-dom';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    //bring method into App comp
+    this.addFish = this.addFish.bind(this);
+
+    //initiate state (fmly "getInitialState")
+    this.state = {
+      fishes:{},
+      order: {}
+    };
+  }
+
+  addFish(fish) {
+    //14:00 13/30
+    //copy current (existing) state & update 
+    //spread {...} takes every item from our object and spreads it into the object
+    const fishes = {...this.state.fishes};
+    //add in new fish w/unique key - timestamp is incremental
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+
+    //alt (poorer performance):
+    //this.state.fishes.fish1 = fish;
+
+    //set state (specific)
+    //{fishes:fishes == fishes} when they are the same
+    this.setState({ fishes })
+
+  }
+
 	render() {
 		return (
 			<div className="catch-of-the-day"> 
@@ -13,7 +44,8 @@ class App extends React.Component {
 					<Header pro="prop" bool={true} tagline="Fresh Seafood Market"/>
 				</div>
 				<Order/>
-				<Inventory/>
+      {/*pass the fn (as PROP) down to the comp where you want to use the method(call it)*/}
+				<Inventory addFish={this.addFish} />
 			</div>
 		)
 	}
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -6,7 +6,8 @@ class Inventory extends React.Component {
 		return(
 			<div>
         <h2>Inventory</h2>
-        <AddFishForm/>   
+      {/*method must be passed down one more level*/}
+        <AddFishForm addFish={this.props.addFish}/>   
       </div>
 		)
 	}
