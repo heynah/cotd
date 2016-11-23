cb3dd9062226bffbfcb0f9f168e0ba0b30ec159d
heynah <hannanicol@gmail.com>
2016-11-22 20:26:21 -0500

    Formatting to <Fish />

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import AddFishForm from './AddFishForm';
+import { formatPrice } from '../helpers';
 
 class Fish extends React.Component {
 	render() {
@@ -12,8 +12,9 @@ class Fish extends React.Component {
         <img src={details.image} alt={details.name}/> {/*no quotes to pass variable inside curlies*/}
         <h3 className="fish-name">
           {details.name}
-          <span className="price">{details.price}</span>
+          <span className="price">{formatPrice(details.price)}</span>
           <p>{details.desc}</p>
+          <button>Add To Order</button>
         </h3>
       </li>
 		)
